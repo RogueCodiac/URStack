4ab6435b2bdb2dcb78f631444aa9de680687a69e
RogueManiac <70204504+RogueManiac@users.noreply.github.com>
2023-05-02 19:37:09 +0300

    Initial commit

---
+++ URStack.cpp
@@ -0,0 +1,386 @@
+/*
+ * StringBuilder Project
+ *
+ *
+ * *.cpp
+ *
+ * Date:        13/04/2023
+ *
+ * Author:      Mahmoud Yaman Seraj Alddin
+ *
+ * Purpose:     Implementation of the Node class, described in Node.h
+ *
+ * List of public Functions:
+ *      Node()
+ *          No-arg constructor of the Node class.
+ *
+ *      Node(const DataType&)
+ *          Parameterized constructor of the Node class.
+ *
+ *      Node& operator=(const Node&)
+ *          Assignment operator for the Node class.
+ *
+ *      ~Node()
+ *          Destructor for the Node class.
+ *
+ *      NodePtr getNext() const
+ *          Returns the pointer to the next Node instance.
+ *
+ *      DataType getData() const
+ *          Returns the data stored in the Node instance.
+ *
+ *      void chain(NodePtr)
+ *          Assigns the next Node instance.
+ *
+ *      void setData(const DataType&)
+ *          Assigns the data field in the Node instance.
+ *
+ *      NodePtr unchain(int)
+ *          Deletes a given number of Node instances following a Node instance.
+ *          Returns a pointer to the new next Node instance.
+ *
+ *      NodePtr skip(int)
+ *          Returns a pointer to a Node,
+ *          after performing a given number of hops.
+ */
+
+
+#include "URStack.h"
+
+#include <utility>
+#include <iostream>
+#include <exception>
+
+using std::string, std::ostream, std::min,
+        std::invalid_argument, std::cout, std::endl;
+
+/*
+ * Pre-Conditions:
+ *      const reference to a message to be displayed.
+ *      ostream reference (optional), default is cout.
+ *
+ * Post-Conditions:
+ *      Displays an error message.
+ *
+ * Displays an error message.
+ */
+void displayInvalidMessage(const string& msg, ostream& out = cout) {
+    out << '\n'
+    << "\033[31;1;1m"   /* Text color becomes red, underlined, bold */
+    << msg
+    << "\033[0m"        /* Text color becomes normal */
+    << endl;
+}
+
+/*
+ * Pre-Conditions:
+ *      const reference to a message to be displayed.
+ *      ostream reference (optional), default is cout.
+ *
+ * Post-Conditions:
+ *      Displays an information message.
+ *
+ * Displays an information message.
+ */
+void displayDataMessage(const string& msg, ostream& out = cout) {
+    out << '\n'
+    << "\033[33;1;1m"   /* Text becomes yellow, bold */
+    << msg
+    << "\033[0m"        /* Text becomes normal */
+    << endl;
+}
+
+/*
+ * Pre-Conditions:
+ *      No preconditions.
+ *
+ * Post-Conditions:
+ *      A default Node instance is created.
+ *      next is initialized to nullptr.
+ *      data is initialized to default value of the type.
+ *
+ * No-arg constructor of the Node class.
+ */
+URStack::Node::Node(): data{}, next{nullptr}, prev{nullptr} {}
+
+/*
+ * Pre-Conditions:
+ *      A const Reference to a variable of type `DataType` is given.
+ *
+ * Post-Conditions:
+ *      A Node instance with the given data is created.
+ *      next is initialized to nullptr.
+ *      data is initialized to the given value.
+ *
+ * Marked explicit to prevent implicit conversions from char* to string.
+ * Parameterized constructor of the Node class,
+ * that takes the data to be stored.
+ */
+URStack::Node::Node(DataType data): data{std::move(data)},
+                                    next{nullptr}, prev{nullptr} {}
+
+/*
+ * Pre-Conditions:
+ *      `this` Node instance & the given Node instance are initialized.
+ *      A const Reference to a Node instance is given from which to copy.
+ *
+ * Post-Conditions:
+ *      data is a copy of the given Node's data.
+ *      next is a copy of the given Node's next.
+ *      Returns a reference to this.
+ *
+ * Assignment operator for the Node class that copies the data fields of
+ * the given Node to this.
+ * data & next data fields are copied using their assignment operators.
+ * If the pointer to other is the same as this, no copying is performed,
+ * prevents self assignment & needles copying.
+ */
+URStack::Node& URStack::Node::operator=(const Node& other) {
+    /* Self-assignment test */
+    if (this == &other) {
+        return *this;
+    }
+
+    data = other.data;
+    next = other.next;
+    prev = other.prev;
+
+    return *this;
+}
+
+/*
+ * Pre-Conditions:
+ *      `this` Node instance is not destroyed.
+ *
+ * Post-Conditions:
+ *      `this` Node instance is destroyed.
+ *      next is nullptr
+ *
+ * Destructor for the Node class.
+ * Deletes all the following Node instances, till it reaches a nullptr.
+ */
+URStack::Node::~Node() {
+    /* Deleting nullptr has no effect */
+    delete next;
+
+    /* Provides protection against illegal access */
+    next = nullptr;
+
+    /* Provides protection against illegal access */
+    prev = nullptr;
+}
+
+/*
+ * Pre-Conditions:
+ *      `this` Node instance is initialized.
+ *
+ * Post-Conditions:
+ *      A Node pointer to the next Node or nullptr is returned.
+ *      No changes to this.
+ *
+ * Marked [[nodiscard]] to allow the compiler to issue warnings in case of
+ * wasteful calls. For example `node.getNext();`.
+ * Returns a copy of the pointer to the next Node instance.
+ */
+URStack::NodePtr URStack::Node::getNext() const {
+    return next;
+}
+
+/*
+ * Pre-Conditions:
+ *      `this` Node instance is initialized.
+ *
+ * Post-Conditions:
+ *      Data of type `DataType` is returned.
+ *      No changes to this.
+ *
+ * Marked [[nodiscard]] to allow the compiler to issue warnings in case of
+ * wasteful calls. For example `node.getData();`.
+ * Returns a copy of the data stored in the Node instance.
+ */
+URStack::Node::DataType URStack::Node::getData() const {
+    return data;
+}
+
+/*
+ * Pre-Conditions:
+ *      `this` Node instance is initialized.
+ *
+ * Post-Conditions:
+ *      A Node pointer to the next Node or nullptr is returned.
+ *      No changes to this.
+ *
+ * Marked [[nodiscard]] to allow the compiler to issue warnings in case of
+ * wasteful calls. For example `node.getNext();`.
+ * Returns a copy of the pointer to the next Node instance.
+ */
+URStack::NodePtr URStack::Node::getPrev() const {
+    return prev;
+}
+
+/*
+ * Pre-Conditions:
+ *      `this` Node instance is initialized.
+ *      An int is given that represents the number of hops.
+ *
+ * Post-Conditions:
+ *      A pointer to a Node instance (or nullptr) is returned,
+ *      points to the instance n-Nodes away.
+ *
+ * Marked [[nodiscard]] to allow the compiler to issue warnings in case of
+ * wasteful calls. For example `node.skip(5);`.
+ * If the number of hops exceeds the number of Nodes nullptr is returned.
+ * If the number of hops is 0 or less, `this` is returned.
+ * Returns a pointer to a Node,
+ * after performing the given number of hops.
+ */
+URStack::NodePtr URStack::Node::skip(int n) {
+    Node *result = this;
+
+    /* Perform n-hops */
+    while (result and 0 < n--) {
+        result = result->getNext();
+    }
+
+    return result;
+}
+
+/*
+ * Pre-Conditions:
+ *      `this` Node instance is initialized.
+ *      The given chain end is guaranteed to be present.
+ *
+ * Post-Conditions:
+ *      All n-Nodes following `this` are deleted.
+ *      next points to the node after the last deleted node.
+ *
+ * If the count is 0 or less, no changes occur, next is returned.
+ * Deletes a given number of Node instances following a Node instance.
+ * Returns a pointer to the new next Node instance.
+ */
+int URStack::Node::unchain(Node *chain_end) {
+    if (this == chain_end) {
+        return 0;
+    }
+
+    int counter{1};
+    NodePtr current_node{this};
+
+    /* Used mainly to count the number of deleted Nodes */
+    while (current_node->getNext() != chain_end) {
+        current_node = current_node->getNext();
+        counter++;
+    }
+
+    if (chain_end) {
+        current_node->chain(nullptr);
+    }
+
+    delete this;
+
+    return counter;
+}
+
+/*
+ * Pre-Conditions:
+ *      `this` Node instance is initialized.
+ *      A pointer to a Node is given.
+ *
+ * Post-Conditions:
+ *      next points to the Node represented by the given pointer.
+ *
+ * Assigns the given pointer to next.
+ */
+void URStack::Node::chain(Node *newNext) {
+    next = newNext;
+
+    if (newNext) {
+        prev = newNext->getPrev();
+    }
+}
+
+URStack::URStack(int capacity): size{0}, top{nullptr}, current{nullptr} {
+    if (capacity <= 0) {
+        throw invalid_argument("\nCapacity must be a positive integer.\n");
+    }
+
+    this->capacity = capacity;
+}
+
+void URStack::InsertNewAction(const string& action) {
+    auto new_action = new Node{action};
+    new_action->chain(current);
+
+    if (top) {
+        size -= top->unchain(current);
+    }
+
+    top = current = new_action;
+
+    if (isFull()) {
+        /* Keeps the junk Node */
+        delete current->skip(size);
+    } else {
+        size++;
+    }
+}
+
+void URStack::undo(ostream& out) {
+    if (!isEmpty()) {
+        displayDataMessage("Undid: " + current->getData(), out);
+
+        if (size != 1) {
+            current = current->getNext();
+        }
+
+        size--;
+    } else {
+        displayInvalidMessage("No actions\a", out);
+    }
+}
+
+void URStack::redo(std::ostream &out) {
+    if (!isEmpty() and current->getPrev()) {
+        current = current->getPrev();
+        displayDataMessage("Redid: " + current->getData(), out);
+        size++;
+    } else {
+        displayInvalidMessage("No previous actions\a", out);
+    }
+}
+
+ostream& URStack::displayDirectional(std::ostream& out, bool to_next) const {
+    NodePtr current_cpy = to_next ? current : current->getPrev();
+
+    displayDataMessage(current_cpy->getData());
+    current_cpy = to_next ? current_cpy->getNext() : current_cpy->getPrev();
+
+    while (current_cpy) {
+        displayDataMessage(" | " + current_cpy->getData(), out);
+        current_cpy = to_next ? current_cpy->getNext() : current_cpy->getPrev();
+    }
+
+    return out;
+}
+
+ostream& URStack::displayAll(std::ostream& out) const {
+    NodePtr top_cpy = top;
+
+    displayDataMessage(top_cpy->getData());
+    top_cpy = top_cpy->getNext();
+
+    while (top_cpy) {
+        displayDataMessage(" | " + top_cpy->getData());
+        top_cpy = top_cpy->getNext();
+    }
+
+    return out;
+}
+
+ostream& URStack::displayPrevious(std::ostream& out) const {
+    return displayDirectional(out, false);
+}
+
+ostream& URStack::displayNext(std::ostream& out) const {
+    return displayDirectional(out, true);
+}
---
+++ URStack.h
@@ -0,0 +1,231 @@
+//
+// Created by Yaman SirajAldeen on 5/1/23.
+//
+
+#ifndef URSTACK_URSTACK_H
+#define URSTACK_URSTACK_H
+
+#include <string>
+#include <iostream>
+
+
+class URStack {
+private:
+    /*
+     * Node class used in MyStringBuilder class to manage single characters.
+     */
+    class Node {
+    public:
+        /*
+         * Type alias for the type of data stored in the Nodes class.
+         * For the use case in MyStringBuilder, char is the type of the data.
+         * Can be accessed outside the class using Node::DataType.
+         */
+        typedef std::string DataType;
+
+        /*
+         * Pre-Conditions:
+         *      No preconditions.
+         *
+         * Post-Conditions:
+         *      A default Node instance is created.
+         *      next is nullptr.
+         *      data is default value of the type.
+         *
+         * No-arg constructor of the Node class.
+         */
+        Node();
+
+        /*
+         * Pre-Conditions:
+         *      A const Reference to a variable of type `DataType` is given.
+         *
+         * Post-Conditions:
+         *      A Node instance with the given data is created.
+         *      next is nullptr.
+         *      data is a copy of the given value.
+         *
+         * Parameterized constructor of the Node class,
+         * that takes the data to be stored.
+         */
+        explicit Node(DataType);
+
+        /*
+         * Pre-Conditions:
+         *      `this` Node instance & the given Node instance are initialized.
+         *      A const Reference to a Node instance is given from which to copy.
+         *
+         * Post-Conditions:
+         *      data is a copy of the given Node's data.
+         *      next is a copy of the given Node's next.
+         *      Returns a reference to this.
+         *
+         * Assignment operator for the Node class that copies the data fields of
+         * the given Node to this.
+         */
+        Node& operator=(const Node&);
+
+        /*
+         * Pre-Conditions:
+         *      `this` Node instance is not destroyed.
+         *
+         * Post-Conditions:
+         *      `this` Node instance is destroyed.
+         *      next is nullptr.
+         *
+         * Destructor for the Node class.
+         */
+        ~Node();
+
+        /*
+         * Pre-Conditions:
+         *      `this` Node instance is initialized.
+         *
+         * Post-Conditions:
+         *      A Node pointer to the next Node or nullptr is returned.
+         *      No changes to this.
+         *
+         * Returns a copy of the pointer to the next Node instance.
+         */
+        [[nodiscard]] Node* getNext() const;
+
+        /*
+         * Pre-Conditions:
+         *      `this` Node instance is initialized.
+         *
+         * Post-Conditions:
+         *      Data of type `DataType` is returned.
+         *      No changes to this.
+         *
+         * Returns a copy of the data stored in the Node instance.
+         */
+        [[nodiscard]] DataType getData() const;
+
+        /*
+         * Pre-Conditions:
+         *      `this` Node instance is initialized.
+         *
+         * Post-Conditions:
+         *      A Node pointer to the next Node or nullptr is returned.
+         *      No changes to this.
+         *
+         * Returns a copy of the pointer to the next Node instance.
+         */
+        [[nodiscard]] Node* getPrev() const;
+
+
+        /*
+         * Pre-Conditions:
+         *      `this` Node instance is initialized.
+         *      An int is given that represents the number of hops.
+         *
+         * Post-Conditions:
+         *      A pointer to a Node instance (or nullptr) is returned,
+         *      points to the instance n-Nodes away.
+         *
+         * Returns a pointer to a Node,
+         * after performing the given number of hops.
+         */
+        [[nodiscard]] Node* skip(int);
+
+        /*
+         * Pre-Conditions:
+         *      `this` Node instance is initialized.
+         *      An int is given that represents the number of Nodes to delete,
+         *      after this.
+         *
+         * Post-Conditions:
+         *      All n-Nodes following `this` are deleted.
+         *      next points to the node after the last deleted node.
+         *
+         * Deletes a given number of Node instances following a Node instance.
+         * Returns a pointer to the new next Node instance.
+         */
+        int unchain(Node*);
+
+        /*
+         * Pre-Conditions:
+         *      `this` Node instance is initialized.
+         *      A pointer to a Node is given.
+         *
+         * Post-Conditions:
+         *      next points to the Node represented by the given pointer.
+         *
+         * Assigns the given pointer to next.
+         */
+        void chain(Node*);
+    private:
+        /*
+         * Pointer to the next Node.
+         * Initialized to nullptr by default.
+         */
+        Node *next;
+
+        /*
+         * Pointer to the previous Node.
+         * Initialized to nullptr by default.
+         */
+        Node *prev;
+
+        /*
+         * Data stored in the Node instance.
+         * Initialized to the default value of the type.
+         */
+        DataType data;
+    };
+
+    /*
+     * Type alias for Node*.
+     * Can be accessed in the implementation using URStack::NodePtr.
+     */
+    typedef Node* NodePtr;
+
+    /*
+     * Pointer to the top Node instance of the URStack instance.
+     * Default is nullptr
+     */
+    NodePtr top;
+
+    /*
+     * Pointer to the current Node instance of the URStack instance.
+     * Default is a NodePtr with an empty string.
+     */
+    NodePtr current;
+
+    /*
+     * Integer representing the maximum number of actions allowed to be
+     * saved in the URStack instance.
+     * Default is 20.
+     */
+    int capacity;
+
+    /*
+     * Integer representing the actual number of actions saved in the
+     * URStack instance (starting from the current NodePtr).
+     * Default is 0.
+     */
+    int size;
+
+    [[nodiscard]] std::ostream& displayDirectional(std::ostream&,
+                                                   bool to_next) const;
+
+    [[nodiscard]] inline bool isFull() const {
+        return size == capacity;
+    }
+
+    [[nodiscard]] inline bool isEmpty() const {
+        return not size;
+    }
+public:
+    explicit URStack(int capacity = 20);
+
+    void InsertNewAction(const std::string&);
+    void undo(std::ostream& out = std::cout);
+    void redo(std::ostream& out = std::cout);
+    [[nodiscard]] std::ostream& displayAll(std::ostream&) const;
+    [[nodiscard]] std::ostream& displayPrevious(std::ostream&) const;
+    [[nodiscard]] std::ostream& displayNext(std::ostream&) const;
+};
+
+
+#endif //URSTACK_URSTACK_H
